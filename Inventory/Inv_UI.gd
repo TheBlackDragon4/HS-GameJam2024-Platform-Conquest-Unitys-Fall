28cea0713ed8b8b98ce2f4259693f3b54e9e41bd
dany-cmd <daniel.schultheiss25@gmx.de>
2024-03-29 23:53:42 +0100

    Inventory v1 completed
    I guess it should be ok.
    Thank you for reading :)

--- Inventory/Inv_UI.gd
+++ Inventory/Inv_UI.gd
@@ -1,16 +1,27 @@
 extends Control
 
+@onready var inv: Inv = preload("res://Inventory/player_inventory.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var inv_open = false
 
 func _ready():
+	update_slots()
 	close()
 
-func _process(delta):
-	if Input.is_action_just_pressed("KEY_DELETE"):
-		if inv_open:
-			close()
-		else:
-			open()
+func update_slots():
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_DELETE:
+			print("DELETE was pressed")
+			if inv_open:
+				close()
+			else:
+				open()
+
 
 func close():
 	inv_open = false
