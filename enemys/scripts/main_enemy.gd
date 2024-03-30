fdfa152b1776d950e869b6789cc4d8af3d9b4d34
BlackDragon <83983652+TheBlackDragon4@users.noreply.github.com>
2024-03-30 18:33:30 +0100

    Add enemy movement

--- enemys/scripts/main_enemy.gd
+++ enemys/scripts/main_enemy.gd
@@ -1,38 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 250.0
-@export var damage = 10.0
-@export var aps = 2.0
+var speed = 100
+const GRAVITY = 32
 
-var attack_speed
-var time_until_attack
-var within_attack_range = false 
+func _physics_process(delta):	
+	move()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	get_tree().root.get_node("CharacterBody2D")
-	attack_speed = 1/aps
-	time_until_attack = attack_speed
-	#print(CharacterBody2D.name())
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if within_attack_range and time_until_attack <= 0:
-		_attack();
-		time_until_attack = attack_speed
-	else:
-		time_until_attack -= delta
-
-func _attack():
-	print("Attack player")
-
-func _on_attack_range_body_enter(body):
-	if body.is_in_group("CharacterBody2D"):
-		print("Player in range")
-		within_attack_range = true
-
-func _on_attack_range_body_exit(body):
-	if body.is_in_group("CharacterBody2D"):
-		within_attack_range = false
-		time_until_attack = attack_speed
+func move():
+	velocity.y += GRAVITY
+	if (velocity.x == 0):
+		speed *= -1
+		velocity.x = speed
+	move_and_slide()
 
