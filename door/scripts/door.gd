1e026033eb17cac4ff41d844cc54fd9eeea8e659
BlackDragon <83983652+TheBlackDragon4@users.noreply.github.com>
2024-03-30 11:58:32 +0100

    Add animation for the door prefab

--- door/scripts/door.gd
+++ door/scripts/door.gd
@@ -1,15 +1,21 @@
 extends Area2D
 
 @export var sceneChanger: String = ""
+@onready var aSpride2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass # Replace with function body.	
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("open_door"):
 		for body in get_overlapping_bodies():
 			if body.name == "CharacterBody2D":
-				get_tree().change_scene_to_file(sceneChanger+".tscn")
+				aSpride2D.play("default", 1.0, false)
+				aSpride2D.frame = 0				
+				#get_tree().change_scene_to_file(sceneChanger+".tscn")
 				#get_tree().change_scene_to_file("res://level/level1.tscn")
+
+func _on_animated_sprite_2d_animation_looped():
+	get_tree().change_scene_to_file(sceneChanger+".tscn")
 
