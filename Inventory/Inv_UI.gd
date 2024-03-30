d40582a409227c422f4f19020bb8edad4e41cff3
dany-cmd <daniel.schultheiss25@gmx.de>
2024-03-30 17:00:23 +0100

    Inventory Movement + Keybinds

--- Inventory/Inv_UI.gd
+++ Inventory/Inv_UI.gd
@@ -5,12 +5,16 @@ extends Control
 
 var inv_open = false
 @onready var selectedSlot = $NinePatchRect/GridContainer/Inv_UI_Slot1
-@onready var selectedSlotSprite = $NinePatchRect/GridContainer/Inv_UI_Slot1/Sprite2D
-@onready var container = $NinePatchRect/GridContainer
+#@onready var selectedSlotSprite = $NinePatchRect/GridContainer/Inv_UI_Slot1/Sprite2D
+var slotArray = []
+var currentPos = 0
+
 
 func _ready():
 	update_slots()
 	close()
+	for slot in $NinePatchRect/GridContainer.get_children():
+		slotArray.append(slot)
 
 func update_slots():
 	for i in range(min(inv.items.size(), slots.size())):
@@ -27,28 +31,50 @@ func _input(_event):
 			open()
 	if inv_open:
 		if Input.is_action_just_pressed("left"):
-			selectedSlotSprite.animation = "default"
-			selectedSlot = selectedSlot.get_focus_neighbor(focus_neighbor_left)
+			if !currentPos < 1:
+				selectedSlot.get_node("Sprite2D").animation = "default"
+				currentPos = currentPos-1
+				selectedSlot = slotArray[currentPos]
+				selectedSlot.get_node("Sprite2D").animation = "selected"
+			else:
+				selectedSlot.get_node("Sprite2D").animation = "default"
+				currentPos = currentPos-1+12
+				selectedSlot = slotArray[currentPos]
+				selectedSlot.get_node("Sprite2D").animation = "selected"				
 		if Input.is_action_just_pressed("right"):
-			selectedSlotSprite.animation = "default"
-			#print("Type: ", typeof(selectedSlot), " selectedSlot:", selectedSlot)
-			#print("selectedSlot.get_focus_neighbor(focus_neighbor_right): ",selectedSlot.get_focus_neighbor(SIDE_RIGHT))
-			#print("typeof(selectedSlot.get_focus_neighbor(focus_neighbor_right)): ",typeof(selectedSlot.get_focus_neighbor(SIDE_RIGHT)))
-			selectedSlot = get_node(selectedSlot.focus_neighbor_right)
-			#print($NinePatchRect/GridContainer.get_node(selectedSlot.get_focus_neighbor(SIDE_RIGHT)))
-			#selectedSlot = $NinePatchRect/GridContainer.get_node(selectedSlot.focus_neighbor_right)
-			print(selectedSlot.focus_neighbor_right.get_name(0))
-			selectedSlot = container.getSlot(selectedSlot.focus_neighbor_right)
-			print(selectedSlot)
-			selectedSlot.get_node("Sprite2D").animation = "selected"
-			#selectedSlotSprite.animation = "selected"
-
+			if !currentPos > 10:
+				selectedSlot.get_node("Sprite2D").animation = "default"
+				currentPos = currentPos+1
+				selectedSlot = slotArray[currentPos]
+				selectedSlot.get_node("Sprite2D").animation = "selected"
+			else:
+				selectedSlot.get_node("Sprite2D").animation = "default"
+				currentPos = currentPos+1-12
+				selectedSlot = slotArray[currentPos]
+				selectedSlot.get_node("Sprite2D").animation = "selected"
 		if Input.is_action_just_pressed("jump"):
-			selectedSlotSprite.animation = "default"
-			selectedSlot = selectedSlot.get_focus_neighbor(focus_neighbor_top)
+			if !currentPos < 4:
+				selectedSlot.get_node("Sprite2D").animation = "default"
+				currentPos = currentPos-4
+				selectedSlot = slotArray[currentPos]
+				selectedSlot.get_node("Sprite2D").animation = "selected"
+			else:
+				selectedSlot.get_node("Sprite2D").animation = "default"
+				currentPos = currentPos+12-4
+				selectedSlot = slotArray[currentPos]
+				selectedSlot.get_node("Sprite2D").animation = "selected"
 		if Input.is_action_just_pressed("interact"):
-			selectedSlotSprite.animation = "default"
-			selectedSlot = selectedSlot.get_focus_neighbor(focus_neighbor_bottom)
+			if !currentPos > 7:
+				selectedSlot.get_node("Sprite2D").animation = "default"
+				currentPos = currentPos+4
+				selectedSlot = slotArray[currentPos]
+				selectedSlot.get_node("Sprite2D").animation = "selected"
+			else:
+				selectedSlot.get_node("Sprite2D").animation = "default"
+				currentPos = currentPos-12+4
+				selectedSlot = slotArray[currentPos]
+				selectedSlot.get_node("Sprite2D").animation = "selected"
+				
 
 
 func close():
@@ -61,5 +87,4 @@ func open():
 	inv_open = true
 	visible = true
 	#selectedSlot.get_node("Sprite2D").set_texture(preload(("res://icons/dummy/Selected_Inventory_Tile.png")))
-	selectedSlotSprite.animation = "selected"
-	print(selectedSlotSprite.animation)
+	selectedSlot.get_node("Sprite2D").animation = "selected"
