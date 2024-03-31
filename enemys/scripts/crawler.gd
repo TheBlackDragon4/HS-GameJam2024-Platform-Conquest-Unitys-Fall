be5e00ab696b4392f544cb0ae9bfb27f5b90549b
David Duschek <149827508+davemaster1203@users.noreply.github.com>
2024-03-31 05:20:13 +0200

    added death animation to crawler
    added death animation to crawler

--- enemys/scripts/crawler.gd
+++ enemys/scripts/crawler.gd
@@ -4,6 +4,8 @@ var speed = 100
 const GRAVITY = 32
 @export var health = Global.crawl_enemy_health
 
+@onready var crawler_sprite = $Sprite2D
+
 func _physics_process(_delta):
 	velocity.y += GRAVITY
 	if (velocity.x == 0):
@@ -15,5 +17,6 @@ func attack(damage):
 	health -= damage
 	if health < 0:
 		#TODO:Death Animation
+		crawler_sprite.play("death")
 		queue_free()
 		
