9e39ea467fd664b91a88af153c08bd32b2aa778e
dany-cmd <daniel.schultheiss25@gmx.de>
2024-04-03 15:08:04 +0200

    Python file is almost completed
    - Only the parameters need to be added

--- python scripts/sprite-fuser.py
+++ python scripts/sprite-fuser.py
@@ -1,15 +1,26 @@
 import png
 from PIL import Image  # Bibliothek zum be-/verarbeiten von Bilder
 
-images = ['1 - Kopie.png', '1 - Kopie.png', '1 - Kopie.png', '2.png', '1.png', '2.png', '3.png', '3.png', '3.png']
 
-w, h, p, m = png.Reader(filename=images[4]).read_flat()
-print(w)
+
+images = ['1.png', '1 - Kopie.png', '1.png', '2.png', '1.png', '2.png', '3.png', '3.png', '3.png']
+
+widths = []
+heights = []
+
+for i in range(9):
+    w, h, p, m = png.Reader(filename=images[i]).read_flat()
+    widths.append(w)
+    heights.append(h)
+
+# print(w)
 stand_pixelsize = 25
-width = w + stand_pixelsize * 2
+width = max(widths[0], widths[3], widths[6]) + max(widths[1], widths[4], widths[7]) + max(widths[0], widths[3], widths[6])
 height = width
 
-print(height, width, stand_pixelsize)
+avg_widths = [max(widths[0], widths[3], widths[6]), max(widths[1], widths[4], widths[7]), max(widths[0], widths[3], widths[6])] 
+            #   max(widths[0], widths[3], widths[6]), max(widths[1], widths[4], widths[7]), max(widths[0], widths[3], widths[6]), 
+            #   max(widths[0], widths[3], widths[6]), max(widths[1], widths[4], widths[7]), max(widths[0], widths[3], widths[6])]
 
 new_im = Image.new('RGBA', (width, height))
 
@@ -18,14 +29,27 @@ def copy_image(image, offset_x=0, offset_y=0):
         im = Image.open(image)
         new_im.paste(im, (i + offset_x, offset_y))
 
-copy_image(images[0], 0, 0)
-copy_image(images[1], stand_pixelsize + int((w/stand_pixelsize-1)*stand_pixelsize/2), 0)
-copy_image(images[2], stand_pixelsize + w, 0)
-copy_image(images[3], 0, stand_pixelsize + int((w/stand_pixelsize-1)*stand_pixelsize/2))
-copy_image(images[4], stand_pixelsize, stand_pixelsize)
-copy_image(images[5], stand_pixelsize + w, stand_pixelsize + int((w/stand_pixelsize-1)*stand_pixelsize/2))
-copy_image(images[6], 0, stand_pixelsize + w)
-copy_image(images[7], stand_pixelsize + int((w/stand_pixelsize-1)*stand_pixelsize/2), stand_pixelsize + w)
-copy_image(images[8], stand_pixelsize + w, stand_pixelsize + w)
+for i in range(9):
+    y_offset = 0
+    x_offset = 0
+    if widths[i] != avg_widths[int((i)/3)]:
+        y_offset = int((avg_widths[int((i)/3)]/widths[i]-1)/2*avg_widths[int((i)/3)]-widths[i]*2)
+    if widths[i] != avg_widths[(i)%3]:
+        x_offset = int((avg_widths[(i)%3]/widths[i]-1)/2*avg_widths[(i)%3]-widths[i]*2)
+    x_pre_offset = int(i%3)
+    y_pre_offset = int(i/3)
+    # if x_pre_offset == 0:
+    #     x_offset = x_offset 
+    if x_pre_offset == 1:
+        x_offset = x_offset + avg_widths[0]
+    if x_pre_offset == 2:
+        x_offset = x_offset + avg_widths[0] + avg_widths[1]
+    # if y_pre_offset == 0:
+    #     y_offset = y_offset
+    if y_pre_offset == 1:
+        y_offset = y_offset + avg_widths[0]
+    if y_pre_offset == 2:
+        y_offset = y_offset + avg_widths[0] + avg_widths[1]
+    copy_image(images[i], x_offset, y_offset)
 
 new_im.save('output.png')
